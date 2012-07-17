7100746cb4ded210bc00a6e3f88c1627a5f441b3
Andrew Pogrebennyk <apogrebennyk@sipwise.com>
2012-07-17 13:56:32 +0000

    remove duplicate cseq from notify's xml body for reginfo

--- modules_k/pua_reginfo/usrloc_cb.c
+++ modules_k/pua_reginfo/usrloc_cb.c
@@ -163,11 +163,6 @@ str* build_reginfo_full(urecord_t * record, str uri, ucontact_t* c, int type) {
 			buf_len = snprintf(buf, sizeof(buf), "%.*s", ptr->user_agent.len, ptr->user_agent.s);
 			xmlNewProp(contact_node, BAD_CAST "user_agent", BAD_CAST buf);
 			
-			/* CSeq Attribute */
-			memset(buf, 0, sizeof(buf));
-			buf_len = snprintf(buf, sizeof(buf), "%d", ptr->cseq);
-			xmlNewProp(contact_node, BAD_CAST "cseq", BAD_CAST buf);
-
 			/* URI-Node */
 			memset(buf, 0, sizeof(buf));
 			buf_len = snprintf(buf, sizeof(buf), "%.*s", ptr->c.len, ptr->c.s);
