318bd5795c0f43621ebd903de6ce75af5bfe68d1
Richard Fuchs <rfuchs@sipwise.com>
2013-03-06 11:47:46 -0500

    remove unsupported variable

--- modules/rtpproxy-ng/rtpproxy.c
+++ modules/rtpproxy-ng/rtpproxy.c
@@ -362,7 +362,9 @@ static struct tm_binds tmb;
 /*0-> disabled, 1 ->enabled*/
 unsigned int *natping_state=0;
 
+#if 0
 static str timeout_socket_str = {0, 0};
+#endif
 static pv_elem_t *extra_id_pv = NULL;
 
 static cmd_export_t cmds[] = {
@@ -440,7 +442,9 @@ static param_export_t params[] = {
 	{"rtpproxy_disable_tout", INT_PARAM, &rtpproxy_disable_tout },
 	{"rtpproxy_retr",         INT_PARAM, &rtpproxy_retr         },
 	{"rtpproxy_tout",         INT_PARAM, &rtpproxy_tout         },
+#if 0
 	{"timeout_socket",    	  STR_PARAM, &timeout_socket_str.s  },
+#endif
 	{"extra_id_pv",           STR_PARAM, &extra_id_pv_param.s },
 	{0, 0, 0}
 };
@@ -908,12 +912,14 @@ mod_init(void)
 		if(rtpp_strings[i])
 			pkg_free(rtpp_strings[i]);
 	}
+#if 0
 	if (timeout_socket_str.s==NULL || timeout_socket_str.s[0]==0) {
 		timeout_socket_str.len = 0;
 		timeout_socket_str.s = NULL;
 	} else {
 		timeout_socket_str.len = strlen(timeout_socket_str.s);
 	}
+#endif
 
 	if (extra_id_pv_param.s && *extra_id_pv_param.s) {
 		extra_id_pv_param.len = strlen(extra_id_pv_param.s);
